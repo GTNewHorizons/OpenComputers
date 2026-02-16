5e48c6f4ad563c2a4e975abc401750834718f196
hinyb <40139991+hinyb@users.noreply.github.com>
2026-02-17 06:06:31 +0800

    feat: add support for AE2FC blocks.

---
+++ src/main/scala/li/cil/oc/integration/ae2fc/DriverFluidExportBus.scala
@@ -0,0 +1,54 @@
+package li.cil.oc.integration.ae2fc
+
+import appeng.api.parts.IPartHost
+import appeng.api.storage.data.IAEItemStack
+import com.glodblock.github.common.parts.PartFluidExportBus
+import li.cil.oc.api.driver
+import li.cil.oc.api.driver.{EnvironmentProvider, NamedBlock}
+import li.cil.oc.api.machine.{Arguments, Callback, Context}
+import li.cil.oc.integration.ManagedTileEntityEnvironment
+import li.cil.oc.integration.appeng.internal.PartSharedItemBusBase
+import li.cil.oc.util.ResultWrapper._
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
+import net.minecraftforge.common.util.ForgeDirection
+
+import scala.reflect.ClassTag
+
+object DriverFluidExportBus extends driver.SidedBlock {
+  override def worksWith(world: World, x: Int, y: Int, z: Int, side: ForgeDirection): Boolean =
+    world.getTileEntity(x, y, z) match {
+      case container: IPartHost => ForgeDirection.VALID_DIRECTIONS.map(container.getPart).filter(obj => {
+        obj != null
+      }).exists(_.isInstanceOf[PartFluidExportBus])
+      case _ => false
+    }
+
+  override def createEnvironment(world: World, x: Int, y: Int, z: Int, side: ForgeDirection) = new Environment(world.getTileEntity(x, y, z).asInstanceOf[IPartHost])
+
+  final class Environment(val host: IPartHost)(implicit val tag: ClassTag[PartFluidExportBus]) extends ManagedTileEntityEnvironment[IPartHost](host, "fluid_exportbus") with NamedBlock with PartSharedItemBusBase[PartFluidExportBus] {
+
+    override def preferredName = "fluid_exportbus"
+
+    override def priority = 2
+
+    @Callback(doc = "function(side:number, [ slot:number]):boolean -- Get the configuration of the export bus pointing in the specified direction.")
+    def getExportConfiguration(context: Context, args: Arguments): Array[AnyRef] = result(getPartConfig(context, args))
+
+    @Callback(doc = "function(side:number[, slot:number][, database:address, entry:number]):boolean OR function(side:number[, slot:number][, detail: table):boolean -- Configure the export bus pointing in the specified direction to export item stacks matching the specified descriptor.")
+    def setExportConfiguration(context: Context, args: Arguments): Array[AnyRef] = {
+      setPartConfig[IAEItemStack](context, args)
+      result(true)
+    }
+
+    @Callback(doc = "function(side:number):number -- Get the number of valid slots in this export bus.")
+    def getExportSlotSize(context: Context, args: Arguments): Array[AnyRef] = result(getSlotSize(context, args))
+  }
+
+  object Provider extends EnvironmentProvider {
+    override def getEnvironment(stack: ItemStack): Class[_] =
+      if (Ae2FcUtil.isFluidExportBus(stack))
+        classOf[Environment]
+      else null
+  }
+}
