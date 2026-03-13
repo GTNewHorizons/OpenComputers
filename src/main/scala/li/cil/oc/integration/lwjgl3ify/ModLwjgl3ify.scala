b40121effcea728574f9ea1fe8e59922d53de38b
hinyb <40139991+hinyb@users.noreply.github.com>
2026-03-13 20:31:08 +0800

    build: fix build conflicts by isolating lwjgl3 sourceSet

--- src/main/scala/li/cil/oc/integration/lwjgl3ify/ModLwjgl3ify.scala
+++ src/main/scala/li/cil/oc/integration/lwjgl3ify/ModLwjgl3ify.scala
@@ -1,12 +1,18 @@
 package li.cil.oc.integration.lwjgl3ify
 
-import li.cil.oc.integration.ModProxy
-import li.cil.oc.integration.Mods
+import li.cil.oc.integration.{ModProxy, Mods}
 
 object ModLwjgl3ify extends ModProxy {
   override def getMod: Mods.SimpleMod = Mods.lwjgl3ify
 
   override def initialize(): Unit = {
-    FileUploadSupport.init()
+    try {
+      val clazz = Class.forName("li.cil.oc.integration.lwjgl3ify.FileUploadSupport")
+      val support = clazz.getDeclaredConstructor().newInstance().asInstanceOf[IFileUpload]
+
+      support.init()
+    } catch {
+      case _: Throwable =>
+    }
   }
 }
