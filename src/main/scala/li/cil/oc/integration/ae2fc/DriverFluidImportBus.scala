6782e15f37a3dfd4dfabac006f471e03bf7506ea
hinyb <40139991+hinyb@users.noreply.github.com>
2026-04-04 09:43:18 -0400

    feat(appeng): integrate StackApi for full fluid and essentia support. (#169)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Kogepan229 <73163994+Kogepan229@users.noreply.github.com>

--- src/main/scala/li/cil/oc/integration/ae2fc/DriverFluidImportBus.scala
+++ src/main/scala/li/cil/oc/integration/ae2fc/DriverFluidImportBus.scala
@@ -1,32 +1,32 @@
-package li.cil.oc.integration.appeng
+package li.cil.oc.integration.ae2fc
 
 import appeng.api.parts.IPartHost
-import appeng.api.storage.data.IAEItemStack
-import appeng.parts.automation.PartImportBus
+import appeng.api.storage.data.IAEFluidStack
+import com.glodblock.github.common.parts.PartFluidImportBus
 import li.cil.oc.api.driver
 import li.cil.oc.api.driver.{EnvironmentProvider, NamedBlock}
 import li.cil.oc.api.machine.{Arguments, Callback, Context}
 import li.cil.oc.integration.ManagedTileEntityEnvironment
-import li.cil.oc.integration.appeng.internal.PartItemBusBase
+import li.cil.oc.integration.appeng.internal.PartSharedItemBusBase
 import net.minecraft.item.ItemStack
 import net.minecraft.world.World
 import net.minecraftforge.common.util.ForgeDirection
 
 import scala.reflect.ClassTag
 
-object DriverImportBus extends driver.SidedBlock {
+object DriverFluidImportBus extends driver.SidedBlock {
   override def worksWith(world: World, x: Int, y: Int, z: Int, side: ForgeDirection) =
     world.getTileEntity(x, y, z) match {
       case container: IPartHost => ForgeDirection.VALID_DIRECTIONS.map(container.getPart).filter(obj => {
         obj != null
-      }).exists(_.isInstanceOf[PartImportBus])
+      }).exists(_.isInstanceOf[PartFluidImportBus])
       case _ => false
     }
 
   override def createEnvironment(world: World, x: Int, y: Int, z: Int, side: ForgeDirection) = new Environment(world.getTileEntity(x, y, z).asInstanceOf[IPartHost])
 
-  final class Environment(val host: IPartHost)(implicit val tag: ClassTag[PartImportBus]) extends ManagedTileEntityEnvironment[IPartHost](host, "me_importbus") with NamedBlock with PartItemBusBase[PartImportBus] {
-    override def preferredName = "me_importbus"
+  final class Environment(val host: IPartHost)(implicit val tag: ClassTag[PartFluidImportBus]) extends ManagedTileEntityEnvironment[IPartHost](host, "fluid_importbus") with NamedBlock with PartSharedItemBusBase[PartFluidImportBus] {
+    override def preferredName = "fluid_importbus"
 
     override def priority = 1
 
@@ -34,7 +34,7 @@ object DriverImportBus extends driver.SidedBlock {
     def getImportConfiguration(context: Context, args: Arguments): Array[AnyRef] = this.getPartConfig(context, args)
 
     @Callback(doc = "function(side:number[, slot:number][, database:address, entry:number]):boolean OR function(side:number[, slot:number][, detail:table]):boolean -- Configure the import bus pointing in the specified direction to import item stacks matching the specified descriptor.")
-    def setImportConfiguration(context: Context, args: Arguments): Array[AnyRef] = this.setPartConfig[IAEItemStack](context, args)
+    def setImportConfiguration(context: Context, args: Arguments): Array[AnyRef] = this.setPartConfig[IAEFluidStack](context, args)
 
     @Callback(doc = "function(side:number):number -- Get the number of valid slots in this import bus.")
     def getImportSlotSize(context: Context, args: Arguments): Array[AnyRef] = getSlotSize(context, args)
@@ -48,7 +48,7 @@ object DriverImportBus extends driver.SidedBlock {
 
   object Provider extends EnvironmentProvider {
     override def getEnvironment(stack: ItemStack): Class[_] =
-      if (AEUtil.isImportBus(stack))
+      if (Ae2FcUtil.isFluidImportBus(stack))
         classOf[Environment]
       else null
   }
