093ec4fe236382e054f28b1ba740c91ba19f4d81
hxync <2391640938@qq.com>
2026-09-04 13:52:25 +0800

    Add three APIs

--- src/main/scala/li/cil/oc/integration/gregtech/DriverBECDiode.java
+++ src/main/scala/li/cil/oc/integration/gregtech/DriverBECDiode.java
@@ -17,6 +17,11 @@ import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import li.cil.oc.integration.ManagedTileEntityEnvironment;
 import tectech.thing.metaTileEntity.multi.bec.MTEBECDiode;
 
@@ -56,6 +61,48 @@ public final class DriverBECDiode extends DriverSidedTileEntity {
     @Override
     public int priority() {
       return 10;
+    }
+
+    @Callback(doc = "function():number -- Returns the number of condensate filter slots.")
+    public Object[] getCondensateFilterCount(Context context, Arguments args) {
+      return new Object[] { tileEntity.getCondensateFilterCount() };
+    }
+
+    @Callback(doc = "function():table -- Returns the condensate filters by slot, using nil for empty slots.")
+    public Object[] getCondensateFilters(Context context, Arguments args) {
+      Map<Integer, String> filters = new HashMap<>();
+      List<Fluid> values = tileEntity.getCondensateFilters();
+      for (int i = 0; i < values.size(); i++) {
+        Fluid fluid = values.get(i);
+        filters.put(i + 1, fluid == null ? null : FluidRegistry.getFluidName(fluid));
+      }
+      return new Object[] { filters };
+    }
+
+    @Callback(doc = "function(filters:table) -- Sets the condensate filters by slot to fluid names, or nil for empty slots.")
+    public Object[] setCondensateFilters(Context context, Arguments args) {
+      Map filters = args.checkTable(0);
+      int filterCount = tileEntity.getCondensateFilterCount();
+      List<Fluid> values = new ArrayList<>(filterCount);
+
+      for (int i = 1; i <= filterCount; i++) {
+        Object value = filters.get(i);
+        if (value == null) {
+          values.add(null);
+          continue;
+        }
+        if (!(value instanceof String)) {
+          throw new IllegalArgumentException("Filter " + i + " must be a fluid name or nil");
+        }
+        Fluid fluid = FluidRegistry.getFluid((String) value);
+        if (fluid == null) {
+          throw new IllegalArgumentException("Unknown fluid: " + value);
+        }
+        values.add(fluid);
+      }
+
+      tileEntity.setCondensateFilters(values);
+      return null;
     }
 
     @Callback(doc = "function(slot:number):string -- Returns the name of the fluid used as condensate filter in the given slot, or nil if no filter is set.")
